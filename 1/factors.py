663d3d26189dd1cc07a1ff9575aea190e681a1f6
ilyakmet <ilyakmet@gmail.com>
2016-09-16 14:41:57 +0300

    v0.1

---
+++ 1/factors.py
@@ -0,0 +1,41 @@
+﻿# import functools
+
+def factors(n):
+    result = []
+    ...
+    return result
+        
+def is_prime(n):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+    
+def mul(lst):
+    result = 1
+    for x in lst:
+        result *= x
+    return result
+    # could be replaced by
+    # return functools.reduce(lambda x, y: x * y, lst, 1)
+    
+def test_factors(n):
+    n_factors = factors(n)
+    assert mul(n_factors) == n
+    for x in n_factors:
+        assert is_prime(x)
+    # could be replaced by
+    # assert all([is_prime(x) for x in n_factors])
+        
+test_factors(1)
+test_factors(2)
+test_factors(5)
+test_factors(10)
+test_factors(81)
+test_factors(10 ** 6)
+test_factors(7 ** 3 * 43 ** 4 * 67 ** 2)
+test_factors(10 ** 12 + 39)
+test_factors(2 ** 50)
+test_factors(2 ** 10 * 3 ** 20)
---
+++ 1/powmod.py
@@ -0,0 +1,13 @@
+﻿def powmod(x, y, z):
+    return x
+    
+def test_powmod(x, y, z):
+    assert powmod(x, y, z) == pow(x, y, z)
+    
+test_powmod(2, 100, 17)
+test_powmod(10, 36, 239)
+MOD = 10 ** 9 + 7
+test_powmod(123, 12345, MOD)
+test_powmod(5, 10 ** 100, MOD)
+test_powmod(997, 10 ** 200, MOD)
+# test_powmod(239, 10 ** 300, MOD)
